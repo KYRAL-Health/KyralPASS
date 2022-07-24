9b691ce3577660d92b1768efb834d0b04253bde1
Lindsay Sperring <sperringlindsay@gmail.com>
2022-07-25 09:33:05 +1000

    updated contract main

--- contract/main.go
+++ contract/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"log"
+	"os"
+
+	"github.com/hyperledger/fabric-chaincode-go/shim"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 	"github.com/hyperledger/fabric-contract-api-go/metadata"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
+
+type serverConfig struct {
+	CCID    string
+	Address string
+}
 
 func run() error {
 	contract := Contract{
@@ -18,17 +27,45 @@ func run() error {
 			TransactionContextHandler: &TransactionContext{},
 		},
 	}
+	// chaincode, err := contractapi.NewChaincode(&contract)
+
+	// if err != nil {
+	// 	return errors.Wrap(err, "Failed to create chain code")
+	// }
+
+	// // TODO: we need proper naming
+	// chaincode.Info.Title = "Kyral-Contract"
+	// chaincode.Info.Version = "1.0.0"
+
+	// if err := chaincode.Start(); err != nil {
+	// 	return errors.Wrap(err, "Failed to start the chaincode")
+	// }
+
+	config := serverConfig{
+		CCID:    os.Getenv("CHAINCODE_ID"),
+		Address: os.Getenv("CHAINCODE_SERVER_ADDRESS"),
+	}
+
 	chaincode, err := contractapi.NewChaincode(&contract)
 
 	if err != nil {
-		return errors.Wrap(err, "Failed to create chain code")
+		log.Panicf("Failed to create chain code: %s", err)
 	}
 
 	// TODO: we need proper naming
 	chaincode.Info.Title = "Kyral-Contract"
 	chaincode.Info.Version = "1.0.0"
 
-	if err := chaincode.Start(); err != nil {
+	server := &shim.ChaincodeServer{
+		CCID:    config.CCID,
+		Address: config.Address,
+		CC:      chaincode,
+		TLSProps: shim.TLSProperties{
+			Disabled: true,
+		},
+	}
+
+	if err := server.Start(); err != nil {
 		return errors.Wrap(err, "Failed to start the chaincode")
 	}
 
